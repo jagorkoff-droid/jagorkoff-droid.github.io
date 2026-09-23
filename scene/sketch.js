e36ee72a7477a2e168d53f6152e9c3b030f826e4
James Gorkoff <jagorkoff@gmail.com>
2026-09-23 15:25:49 -0600

    Started angry birds game

--- scene/sketch.js
+++ scene/sketch.js
@@ -5,11 +5,29 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+let img, x, y;
+let imageScale = 0.06;
+
 
 async function setup() {
   createCanvas(windowWidth, windowHeight);
+  imageMode(CENTER);
+
+  x = 250;
+  y = 700;
+  img = await loadImage("red_angry_bird.png");
 }
 
 function draw() {
   background(220);
+  displayBird();
+  displaySlingshot();
+}
+
+function displayBird() {
+  image(img, x, y, img.width * imageScale, img.height * imageScale);
+}
+
+function displaySlingshot() {
+  
 }
